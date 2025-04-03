2f42f4e24e8cfe1b754c4b352195474da0b888ea
Mohit Singh Panwar <panw0011@algonquinlive.com>
2025-04-03 18:28:31 -0400

    Update router.js

--- router.js
+++ router.js
@@ -139,7 +139,7 @@ function loadPage(page) {
         // document.title = "Services";
         //document.title.focus();
         returnHTML = `
-                            <img src="images/scheduleacall.jpg"  alt="EmpowerAbilityLab logo"/>
+                            <img src="images/services.jpg"  alt="Thumbsup emoji"/>
                             <h2>Services content architecture</h2>
                             <p>Dedicated space or programs designed to cultivate empathy and understanding among individuals 
                                 towards the challenges faced by people with disabilities in using technology and interacting with 
